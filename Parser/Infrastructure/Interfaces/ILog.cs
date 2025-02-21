e6f19e1300d36be58a4bc3dcb7b032f1860f8ab1
VWO0 <denus.parashencko@gmail.com>
2025-02-21 12:44:22 +0200

    Mail parcer vas added

---
+++ Parser/Infrastructure/Interfaces/ILog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace Parser.Infrastructure.Interfaces
+{
+    public interface ILog
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        public static void Debug(string message) => logger.Debug(message);
+        public static void Info(string message) => logger.Info(message);
+        public static void Warn(string message) => logger.Warn(message);
+        public static void Error(string message) => logger.Error(message);
+        public static void Fatal(string message) => logger.Fatal(message);
+    }
+}
